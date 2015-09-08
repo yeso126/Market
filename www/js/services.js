d034d9985b79dfc7128245c49143abf759342d3d
Yesid Ghisays <yeso126@hotmail.com>
2015-09-08 12:14:29 -0500

    chartDataCacheService finished

--- www/js/services.js
+++ www/js/services.js
@@ -26,6 +26,25 @@ angular.module('market.services',[])
   };
 })
 
+.factory('chartDataCacheService',function(CacheFactory){
+
+  var chartDataCache;
+  if(!CacheFactory.get('chartDataCache')){
+    chartDataCache= CacheFactory('chartDataCache', {
+      maxAge: 60 * 60 * 8 * 1000,
+      deleteOnExpire: 'aggressive',
+      storageMode: 'localStorage'
+    });
+  }
+  else {
+    chartDataCache = CacheFactory.get('chartDataCache');
+  }
+
+
+  return chartDataCache;
+})
+
+
 .factory('stockDataService', function($q, $http, encodeURIService){
 
   var getDetailsData = function(ticker) {
@@ -70,57 +89,66 @@ angular.module('market.services',[])
   };
 })
 
-.factory('chartDataService', function ($q, encodeURIService, $http){
+.factory('chartDataService', function ($q, encodeURIService, $http, chartDataCacheService){
 
   var getHistoricalData= function(ticker, fromDate, todayDate) {
     var deferred = $q.defer(),
+
+    cacheKey = ticker,
+    chartDataCache = chartDataCacheService.get(cacheKey),
+
     query = 'select * from yahoo.finance.historicaldata where symbol = "'+ ticker +'" and startDate = "'+ fromDate +'" and endDate = "'+ todayDate +'"' ,
     url ='http://query.yahooapis.com/v1/public/yql?q=' + encodeURIService.encode(query) + '&format=json&env=http://datatables.org/alltables.env';
 
-    $http.get(url)
-      .success(function(json){
-        var jsonData = json.query.results.quote;
+    if(chartDataCache){
+      deferred.resolve(chartDataCache);
+    }
+    else {
+      $http.get(url)
+        .success(function(json){
+          var jsonData = json.query.results.quote;
 
-        var priceData=[],
-        volumeData = [];
+          var priceData=[],
+          volumeData = [];
 
-        jsonData.forEach(function(dayDataObject){
+          jsonData.forEach(function(dayDataObject){
 
-          var dateToMillis = dayDataObject.Date,
-          date = Date.parse(dateToMillis),
-          price= parseFloat(Math.round(dayDataObject.Close * 100) / 100).toFixed(3),
-          volume = dayDataObject.Volume,
+            var dateToMillis = dayDataObject.Date,
+            date = Date.parse(dateToMillis),
+            price= parseFloat(Math.round(dayDataObject.Close * 100) / 100).toFixed(3),
+            volume = dayDataObject.Volume,
 
-          volumeDatum = '[' + date + ',' + volume + ']',
+            volumeDatum = '[' + date + ',' + volume + ']',
 
-          priceDatum = '[' + date + ',' + price + ']';
-
-
-
-          volumeData.unshift(volumeDatum);
-          priceData.unshift(priceDatum);
+            priceDatum = '[' + date + ',' + price + ']';
 
 
+
+            volumeData.unshift(volumeDatum);
+            priceData.unshift(priceDatum);
+
+
+          });
+
+          var formattedChartData =
+          '[{' +
+            '"key":' + '"volume",' +
+            '"bar":' + 'true,' +
+            '"values":' + '[' + volumeData + ']' +
+          '},' +
+          '{' +
+              '"key":' + '"' + ticker + '",' +
+              '"values":' + '[' + priceData + ']' +
+            '}]';
+
+          deferred.resolve(formattedChartData);
+          chartDataCacheService.put(cacheKey, formattedChartData);
+        })
+        .error(function(error){
+          console.log("chart data error: " + error);
+          deferred.reject();
         });
-
-        var formattedChartData =
-        '[{' +
-          '"key":' + '"volume",' +
-          '"bar":' + 'true,' +
-          '"values":' + '[' + volumeData + ']' +
-        '},' +
-        '{' +
-            '"key":' + '"' + ticker + '",' +
-            '"values":' + '[' + priceData + ']' +
-          '}]';
-
-        deferred.resolve(formattedChartData);
-      })
-      .error(function(error){
-        console.log("chart data error: " + error);
-        deferred.reject();
-      });
-
+    }
     return deferred.promise;
   };
 
